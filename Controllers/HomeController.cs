2063f39ffd0aa8e0c6853918e7dc14671fc374d6
Mikoláš Fromm <mikolas.fromm@gmail.com>
2023-08-27 14:38:44 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebWhisperer.Models;
+using WebWhisperer.Services;
+
+namespace WebWhisperer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    [Route("api/whisper")]
+    [ApiController]
+    public class WhisperController : ControllerBase
+    {
+        private readonly WhisperService _whisperService;
+
+        public WhisperController(WhisperService whisperService)
+        {
+            _whisperService = whisperService;
+        }
+
+        [HttpPost]
+        [Route("process")]
+        public ActionResult<List<string>> ProcessInput([FromBody] string inputChar)
+        {
+            List<string> whisperText = _whisperService.ProcessInput(inputChar);
+            return Ok(whisperText);
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public ActionResult LoadUserInput([FromBody] string userInput)
+        {
+            _whisperService.LoadUserInput(userInput);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("init-query")]
+        public ActionResult<string> GetInitQuery()
+        {
+            string result = _whisperService.GetInitQuery();
+            return Ok(result);
+        }
+    }
+}
---
+++ Services/WhisperService.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace WebWhisperer.Services
+{
+    public class WhisperService
+    {
+        private string _userInput = string.Empty;
+
+        private string _querySoFar = string.Empty;
+
+        private bool _whisperEnabled = false;
+        private StringBuilder _whisperText = new StringBuilder();
+
+        public List<string> ProcessInput(string inputChar)
+        {
+            return new List<string> { "X", "Y", "Z" };
+            //if (inputChar == '.')
+            //{
+            //    _whisperEnabled = !_whisperEnabled; // Toggle whisper mode
+            //}
+            //else if (_whisperEnabled)
+            //{
+            //    _whisperText.Append(char.ToLower(inputChar));
+            //}
+
+            //return _whisperText.ToString();
+        }
+
+        public void LoadUserInput(string userInput)
+        {
+            // load new user input
+            _userInput = userInput;
+
+            // clear the previous query
+            _querySoFar = string.Empty;
+        }
+
+        public string GetInitQuery()
+        {
+            return "Init";
+        }
+    }
+
+}
